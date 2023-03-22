296ebbd80298eebd45401e5698d456f5f413278e
import-logging <128501047+import-logging@users.noreply.github.com>
2023-03-22 14:57:38 +0000

    Test

--- src/web/app.py
+++ src/web/app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
-from db import get_db, close_db
-import sqlalchemy
 from logger import log
 
 app = Flask(__name__)
-app.teardown_appcontext(close_db)
-
 
 @app.route("/")
 def index():
