7b984dce461a0071d13c0c27c2c450701d6f94f8
import-logging <128501047+import-logging@users.noreply.github.com>
2023-03-22 14:38:48 +0000

    Test

--- src/web/app.py
+++ src/web/app.py
@@ -12,21 +12,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/health")
-def health():
-    log.info("Checking /health")
-    db = get_db()
-    health = "BAD"
-    try:
-        result = db.execute("SELECT NOW()")
-        result = result.one()
-        health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
-    except sqlalchemy.exc.OperationalError as e:
-        msg = f"sqlalchemy.exc.OperationalError: {e}"
-        log.error(msg)
-    except Exception as e:
-        msg = f"Error performing healthcheck: {e}"
-        log.error(msg)
-
-    return health
+@app.route("/test")
+def test():
+    return "Hello World"
